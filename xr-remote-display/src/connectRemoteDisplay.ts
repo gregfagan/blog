025ad68cba378194a45cf805f0b5bcda18c4af0a
Greg Fagan <gregfagan@gmail.com>
2023-04-17 17:35:43 -0500

    tweak connectRemoteDisplay

--- xr-remote-display/src/connectRemoteDisplay.ts
+++ xr-remote-display/src/connectRemoteDisplay.ts
@@ -1,33 +1,31 @@
 const ws = import.meta.hot;
 
 export const video = document.createElement("video");
+video.muted = true;
+video.autoplay = true;
+video.playsInline = true;
+video.style.position = "absolute";
+video.style.width = "100%";
+video.style.height = "100%";
+video.style.opacity = "0";
+document.body.appendChild(video);
 
 export function connect() {
   const pc = new RTCPeerConnection();
-  video.style.position = "absolute";
-  video.style.width = "100%";
-  video.style.height = "100%";
-  video.muted = true;
-  video.autoplay = true;
-  video.playsInline = true;
-  video.style.opacity = "0";
-  document.body.appendChild(video);
 
   ws?.on("rtc:offer", async (data) => {
-    const offer = JSON.parse(data);
-    pc.setRemoteDescription(offer);
+    pc.setRemoteDescription(JSON.parse(data));
     const answer = await pc.createAnswer();
     pc.setLocalDescription(answer);
     ws.send("rtc:answer", JSON.stringify(answer));
   });
 
-  ws?.on("rtc:ice", async (data) => {
-    const ice = JSON.parse(data);
-    pc.addIceCandidate(new RTCIceCandidate(ice));
-  });
-
   pc.addEventListener("icecandidate", (e) => {
     if (e.candidate) ws?.send("rtc:ice", JSON.stringify(e.candidate));
+  });
+
+  ws?.on("rtc:ice", async (data) => {
+    pc.addIceCandidate(new RTCIceCandidate(JSON.parse(data)));
   });
 
   pc.addEventListener("track", (e) => {
